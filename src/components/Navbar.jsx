f13c20359d0bd57e1aaafafc575b231191f91820
Kheum Sokkhen <kheumsokkhen@Kheums-MacBook-Pro.local>
2026-09-18 18:21:31 +0700

    fix: force git re-index all files and case sensitivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ export default function Navbar({ activeTab, setActiveTab, isPopUpOpen = false })
         className={`w-full bg-white border-b border-neutral-200 sticky top-0 z-50 select-none ${isPopUpOpen ? "pointer-events-none opacity-50" : ""
           }`}
       >
-        <div className="w-full px-4 md:px-12 py-3 flex items-center justify-between bg-white relative z-50">
+        <div className="w-full px-5 md:px-12 py-3 flex items-center justify-between bg-white relative z-50">
           {/* Mobile Menu Icon */}
           <div className="flex items-center md:hidden">
             <button
